1def7a768f3e48332c42c9eac4f3785196d610e2
Akagaku <121218917+Akagaku@users.noreply.github.com>
2022-12-25 22:36:44 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".drop-div").slideUp(0);
+
+    $(".drop-btn").on("click", function (event) {
+        $(".drop-" + event.currentTarget.id).slideToggle();
+    });
+});
