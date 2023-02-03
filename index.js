692a04a129166314c128b3f143cfb4c3725c643c
Akagaku <akagaku@proton.me>
2023-02-03 15:45:24 -0800

    so much

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
-    $(function () {
-        $("#navbar").load("/navbar.html");
-      });
+  $("#navbar").load("/navbar.html");
+
+  $(".drop-item").slideUp(0);
+
+    $(".drop-btn").on("mouseenter", function (event) {
+        $(".drop-item:not(.drop-" + event.target.id +")").slideUp(300);
+        $(".drop-" + event.target.id).slideDown(300);
+    });
+
+    $(".drop-div").on("mouseleave", function () {
+        $(".drop-item").slideUp(300);
+    });
+
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 });
